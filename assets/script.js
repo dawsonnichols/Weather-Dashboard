0465f60db3b5da8afdc8bb9b44bf506e0aabbe9b
dawsonnichols <85593807+dawsonnichols@users.noreply.github.com>
2022-04-26 22:57:27 -0700

    geo api

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,49 @@
 // add city search function
-window.onload=function() {
-var searchBtn = document.getElementById("#searchBtn");
-var cityInput = document.getElementById("#textAreaInput")
-var temp = document.getElementById("#temp")
-var wind = document.getElementById("#wind")
-var humdidity = document.getElementById("#humidity")
-var uv = document.getElementById("#uv")
-}
+
+var searchBtn = document.getElementById("searchBtn");
+var cityInput = document.getElementById("textAreaInput")
+var cityDisplay = document.getElementById("cityName")
+var temp = document.getElementById("temp")
+var wind = document.getElementById("wind")
+var humidity = document.getElementById("humidity")
+var uv = document.getElementById("uv")
+var coords; 
 // put api information of currently selected city into main box
 
 searchBtn.addEventListener("click",function() {
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&appid=b497240b0e51a78d8e70e5314eea6385') 
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-.catch(err => alert("Does not exist"))
+fetch('http://api.openweathermap.org/geo/1.0/direct?q='+cityInput.value+'&limit=1&appid=b497240b0e51a78d8e70e5314eea6385').then(function
+(response) {
+    if (response.ok) {
+        return response.json(); 
+    } else {
+        return Promise.reject(response); 
+    }
+}).then(function (data) {
+    console.log(data)
+    coords = data; 
+    console.log(coords); 
 })
+    
+//     return fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=b497240b0e51a78d8e70e5314eea6385')
+// }
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data); 
+//         var cityName = data['name'];
+//         var temperatureData = data['main']['temp']; 
+//         var windData = data['wind']['speed']; 
+//         var humidityData = data['main']['humidity']; 
+//         var uvData = data['']
+
+//         cityDisplay.innerHTML = cityName;
+//         temp.innerHTML = temperatureData; 
+//         wind.innerHTML = windData; 
+//         humidity.innerHTML = humidityData; 
+
+//     })
+
+
+// })
 // add search history in the form of buttons
 
 
@@ -25,4 +53,5 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&app
 // 5 day forecast for currently selected city
 
 
-// text area js resizing code for city search ui. 
+// text area js resizing code for city search ui.
+});
